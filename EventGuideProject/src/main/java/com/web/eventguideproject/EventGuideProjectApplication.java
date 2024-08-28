9b486a2358a4d597e97832c4cc9d2644a22e094c
seungjae100 <seungjaebaekmail@gmail.com>
2024-08-28 21:19:00 +0900

    develop 브랜치 생성

---
+++ EventGuideProject/src/main/java/com/web/eventguideproject/EventGuideProjectApplication.java
@@ -0,0 +1,13 @@
+package com.web.eventguideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventGuideProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventGuideProjectApplication.class, args);
+    }
+
+}
